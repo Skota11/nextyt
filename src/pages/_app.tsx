d022d4ea305d7d9bb9b8513754ba429967a97882
S Kota <91359399+Skota11@users.noreply.github.com>
2023-06-05 23:41:40 +0900

    AddGTag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,27 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { GA_TRACKING_ID, pageview } from '../libs/gtag';
+
 import Header from "../components/header";
 
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    // GA_TRACKING_ID が設定されていない場合は、処理終了
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return <>
     <Header />
     <Component {...pageProps} />
