a93ad21edf44ecdac5c17035cf368126552b038e
S Kota <91359399+Skota11@users.noreply.github.com>
2023-06-06 15:13:49 +0900

    resetGTag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { GA_TRACKING_ID } from '../libs/gtag';
 
 export default function Document() {
   return (
@@ -8,24 +7,6 @@ export default function Document() {
         <link rel="manifest" href="sw.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
         <meta name="theme-color" content="#fff" />
-
-        {GA_TRACKING_ID && (
-          <>
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-        `,
-              }}
-            />
-          </>
-        )}
       </Head>
       <body>
         <Main />
